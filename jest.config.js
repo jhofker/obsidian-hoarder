b1ffd541848b0ceabf04c154eeade78648dd155d
Jordan Hofker <jhofker@gmail.com>
2026-03-01 17:25:27 -0600

    test: add Jest mock infrastructure for obsidian module

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,7 @@ module.exports = {
     '!src/**/*.d.ts',
   ],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^obsidian$': '<rootDir>/src/__mocks__/obsidian.ts',
+  },
 };
---
+++ src/__mocks__/obsidian.ts
@@ -0,0 +1,10 @@
+export const requestUrl = jest.fn();
+export class App {}
+export class TFile {}
+export class Plugin {}
+export class Events {
+  trigger = jest.fn();
+}
+export class Notice {
+  constructor(_message: string) {}
+}
